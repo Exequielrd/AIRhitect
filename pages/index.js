fac29c6cf92e0f980d148bb83dbcb21d33fe0630
exequiel <exequielrd10@gmail.com>
2021-05-31 01:41:06 -0300

    meta2

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         />
         <meta property="og:title" content="AIRhitect - Inicio" />
         <meta property="og:description" content="hola como estas todo bien" />
-        <meta property="og:image" content="" />
+        <meta property="og:image" content="/meta-index.png" />
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta
@@ -29,7 +29,7 @@ export default function Home() {
           property="twitter:description"
           content="hola como estas todo bien"
         />
-        <meta property="twitter:image" content="" />
+        <meta property="twitter:image" content="/meta-index.png" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <TitleContainer>
